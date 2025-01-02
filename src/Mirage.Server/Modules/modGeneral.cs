2df8beb950f683cd7a33d43c4170385bf30f4bcb
Xlithan <steve.jones487@gmail.com>
2025-01-02 04:25:45 +0000

    Obsolete code

--- src/Mirage.Server/Modules/modGeneral.cs
+++ src/Mirage.Server/Modules/modGeneral.cs
@@ -70,8 +70,6 @@ public static partial class modGeneral
         modTypes.ClearSpells();
         SetStatus("Loading classes...");
         modDatabase.LoadClasses();
-        //SetStatus("Loading old maps...");
-        //modDatabase.LoadOldMaps();
         SetStatus("Loading maps...");
         modDatabase.LoadMaps();
         SetStatus("Loading items...");
